7063ab1973b1494e883a0abbd366b17ca0753558
Silas Pereira <silasrp@gmail.com>
2023-07-05 18:39:34 +0100

    Using Modular components (partial)

--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -1,9 +1,17 @@
 <template>
-    <h1>Products Page</h1>
+    <h1>Products</h1>
+
 </template>
 
 <script>
+import { products } from '../temp-data';
+
 export default {
     name: "ProductsPage",
+    data () {
+        return {
+            products,
+        }
+    }
 }
 </script>
