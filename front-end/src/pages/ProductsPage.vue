46ce23240856badcac57de6bce10370c44421586
Silas Pereira <silasrp@hotmail.com>
2023-07-05 19:17:21 +0100

    Completed frontend

--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -1,13 +1,17 @@
 <template>
     <h1>Products</h1>
-
+    <ProductsList :products="products" />
 </template>
 
 <script>
+import ProductsList from '../components/ProductsList.vue';
 import { products } from '../temp-data';
 
 export default {
     name: "ProductsPage",
+    components: {
+        ProductsList,
+    },
     data () {
         return {
             products,
