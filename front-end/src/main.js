10219744cbf6d5f7b18a82aab47dfa3891eb2c7c
Silas Pereira <silasrp@gmail.com>
2023-07-07 12:08:54 +0100

    Authentication incomplete

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -7,6 +7,20 @@ import ProductsPage from './pages/ProductsPage.vue';
 import ProductDetailPage from './pages/ProductDetailPage.vue';
 import NotFoundPage from './pages/NotFoundPage.vue';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpXenQvv89o4qVCkTnqKRBMraV7Tn9C1w",
+  authDomain: "vue-site-d11a6.firebaseapp.com",
+  projectId: "vue-site-d11a6",
+  storageBucket: "vue-site-d11a6.appspot.com",
+  messagingSenderId: "549154159356",
+  appId: "1:549154159356:web:2a77de1dbb7166462bb579"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 createApp(App)
 .use(VueRouter.createRouter ({
     history: VueRouter.createWebHistory(process.env.BASE_URL),
--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -6,7 +6,9 @@
         <div class="product-details">
             <h1>{{ product.name }}</h1>
             <h3 class="price">{{ product.price }}</h3>
-            <button @click="addToCart" class="add-to-cart">Add to cart</button>
+            <button @click="addToCart" class="add-to-cart" v-if="user && !itemIsInCart">Add to cart</button>
+            <button class="grey-button" v-if="user && itemIsInCart">Item is already in cart</button>
+            <button class="sign-in" @click="signIn" v-if="!user">Sign in to add to cart</button>
         </div>        
     </div>
     <div v-else>
@@ -16,29 +18,71 @@
 </template>
 
 <script>
+import { getAuth, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink } from 'firebase/auth';
 import axios from 'axios';
 import NotFoundPage from './NotFoundPage.vue';
 
 export default {
     name: "ProductDetailPage",
+    props: ['user'],
     data() {
         return {
             product: {},
+            cartItems: [],
+        }
+    },
+    computed: {
+        itemIsInCart() {
+            return this.cartItems.some(item => item.id === this.$route.params.productId);
+        }
+    },
+    watch: {
+        async user(newUserValue) {
+            if (newUserValue) {
+                const cartResponse = await axios.get('/api/users/${newUserValue.uid}/cart/');
+                const cartItems = cartResponse.data;
+                this.cartItems = cartItems;
+            }            
         }
     },
     methods: {
         async addToCart() {
             await axios.post('/api/users/12345/cart', { id: this.$route.params.productId });
             alert('Successfully added item to cart!');
+        },
+        async signIn() {
+            const email = prompt('Pleae enter your email to sign in:');
+            const auth = getAuth();
+            const actionCodeSettings = {
+                url: `https://localhost:8080/products/${this.$route.params.productId}`,
+                handleCodeInApp: true,
+            }
+            await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+            alert('A login link was sent to the email you provided');
+            window.localStorage.setItem('emailForSignIn', email);
         }
     },
     components: {
         NotFoundPage,
     },
     async created() {
+        const auth = getAuth();
+        if (isSignInWithEmailLink(auth, window.location.href)) {
+            const email = window.localStorage.getItem('emailForSignIn');
+            await signInWithEmailLink(auth, email, window.location.href);
+            alert('Successfully signed in!');
+            window.localStorage.removeItem('emailForSignIn');
+        }
         const response = await axios.get(`/api/products/${this.$route.params.productId}`);
         const product = response.data;
         this.product = product;
+
+        if (this.user) {
+            const cartResponse = await axios.get('/api/users/${this.user.uid}/cart/');
+            const cartItems = cartResponse.data;
+            this.cartItems = cartItems;
+        }
+
     }
 }
 </script>
