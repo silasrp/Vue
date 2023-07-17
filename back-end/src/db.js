55d5eb1560c017b42c157d86a922469186f38417
Silas Pereira <silasrp@hotmail.com>
2023-07-18 00:36:57 +0100

    added new connection string

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -3,7 +3,7 @@ let db;
 
 async function  connectToDb(cb){
     try {
-        const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@vue-cluster.hldcook.mongodb.net/`);
+        const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@vue-cluster.hldcook.mongodb.net/?retryWrites=true&w=majority`);
         await client.connect();
 
         db = client.db('full-stack-vue');  //use full-stack-vue    
