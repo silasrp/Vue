c3eafc3986807caa663f0303fd0e93614a2e4ecc
Silas Pereira <silasrp@hotmail.com>
2023-07-18 00:25:29 +0100

    try-catch block unhandled-promise-rejection-nodejs

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -2,12 +2,17 @@ import {MongoClient} from 'mongodb';
 let db;
 
 async function  connectToDb(cb){
-    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@vue-cluster.hldcook.mongodb.net/`);
-    await client.connect();
+    try {
+        const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@vue-cluster.hldcook.mongodb.net/`);
+        await client.connect();
 
-    db = client.db('full-stack-vue');  //use full-stack-vue    
-    cb();
-
+        db = client.db('full-stack-vue');  //use full-stack-vue    
+        cb();
+    }
+    catch(err) {
+        console.error("Something went wrong")
+        console.error(err)
+    }
 }
 export {
     db,
